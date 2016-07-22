e51de0dc31fb3d027ac6044b0d81f7cc68126d7d
tonghonha <honha2412@gmail.com>
2016-07-22 09:35:52 +0700

    Add files via upload

--- log/src/main/java/webUsageTracking/WebTracker.java
+++ log/src/main/java/webUsageTracking/WebTracker.java
@@ -1,6 +1,7 @@
 package webUsageTracking;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -8,14 +9,21 @@ import javax.ws.rs.core.MediaType;
 @Path("/webtracker")
 public class WebTracker {
 
-	@GET
-	@Path("/users")
+	@POST
+	@Path("/userinfo")
 	@Produces(MediaType.APPLICATION_XML)
-	public void getUsers() {
+	public void getUserInfo(String pTitle, String pUrl) {
 		
 		
 		//send web tracking information to RECSYS
-		System.out.print("send web tracking information to RECSYS");
+		System.out.print("send web tracking information to RECSYS\n");
+		String title = pTitle;
+		String url = pUrl;
+		String timestamp = "";
+		String IP = "";
+		
+		System.out.print(title + ":" + pUrl);
+		
 	}
 
 }
